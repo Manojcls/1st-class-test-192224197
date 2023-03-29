bc5a31d2c42d2a52963f30db6f0c91bcd439c0c6
Manojcls <129164180+Manojcls@users.noreply.github.com>
2023-03-29 09:58:30 +0530

    Add files via upload

---
+++ gcc manoj's test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i,n,fact=1;
+    printf("Enter a number: ");
+    scanf("%d", &i);
+
+    for (i=0;i>=n;i--)
+{
+		fact= fact *i;	}    
+printf("\n 192224197");
+    return 0;
+}
